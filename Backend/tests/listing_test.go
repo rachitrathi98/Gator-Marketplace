196829531e7e85733962e44863e8a9fa78725ef7
Rachit Rathi <rachitrathi98@gmail.com>
2022-03-03 21:21:24 -0500

    Add Test Case for POST API of Listing

--- Backend/tests/listing_test.go
+++ Backend/tests/listing_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"GatorMarketPlace/database"
+	"GatorMarketPlace/models"
+	"bytes"
 	"context"
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 	"time"
@@ -10,6 +13,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert" // add Testify package
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestGetListing(t *testing.T) {
@@ -51,6 +55,60 @@ func TestGetListing(t *testing.T) {
 	for _, test := range tests {
 		req := httptest.NewRequest("GET", test.route, nil)
 
+		resp, _ := app.Test(req, 1)
+
+		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
+	}
+}
+
+func TestPostListing(t *testing.T) {
+
+	tests := []struct {
+		description  string // description of the test case
+		route        string // route path to test
+		expectedCode int
+		reqBody      string
+		headers      map[string]string // expected HTTP status code
+	}{
+		// First test case
+		{
+			description:  "POST HTTP status 200",
+			route:        "/post-listing",
+			expectedCode: 200,
+			reqBody:      `{"title": "New Listing", "description" : "This is a new Listing", "tag" : "House", "createdBy" : "61fdd0848cea9423f269f01c", "location" : "Gainesville", "price" : "3"}`,
+			headers:      map[string]string{`Content-Type`: `application/json`},
+		},
+	}
+
+	app := fiber.New()
+
+	// Create route with POST method for test
+	app.Post("/post-listing", func(c *fiber.Ctx) error {
+
+		var listing models.Listing
+		er := c.BodyParser(&listing)
+
+		if er != nil {
+			return c.SendString("Body is Empty")
+		}
+
+		listing.Id = primitive.NewObjectID()
+		listingsCollections := database.MI.Db.Collection("listings")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		_, e := listingsCollections.InsertOne(ctx, listing)
+
+		if e != nil {
+			return e
+		}
+
+		return c.SendStatus(200)
+	})
+
+	for _, test := range tests {
+
+		rbody, _ := json.Marshal(test.reqBody)
+		req := httptest.NewRequest("POST", test.route, bytes.NewReader(rbody))
+		req.Header.Add(`Content-Type`, `application/json`)
 		resp, _ := app.Test(req, 1)
 
 		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
