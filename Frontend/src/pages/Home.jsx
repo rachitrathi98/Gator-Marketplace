6a6b214971ea7b6c60c59bc84d16189ee724455d
MustafaGangardiwala <gangardiwalam.mg@gmail.com>
2022-03-25 18:55:26 -0400

    Added Pagination to Home Page with Search template

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import Card from "../components/Card"
 import axios from "axios"
 import Navbar from "../components/Navbar";
-
+import ReactPaginate from "react-paginate";
+import React from "react";
 
 const Home = () => {
   const[user, setUser]  = useState({})  
   const[listings, setListings] = useState([{}])
+  const [filter_listings, setFilterListings] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listingsPerPage, setListingsPerPage] = useState(2);
+
 
     useEffect(async () => {
      
@@ -24,20 +29,51 @@ const Home = () => {
            let lists = []
            resp.data.listings.map((listing) => lists.push(listing))
            setListings(resp.data.listings)
+           setFilterListings(resp.data.listings)
            console.log(listings)
         }
 
       }, []);
 
+  const searchListing = (newList) => {
+    setFilterListings(newList);
+  };
 
+  const indexOfLastListing = currentPage * listingsPerPage;
+  const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+  const currentListings = filter_listings.slice(
+    indexOfFirstListing,
+    indexOfLastListing,
+  );
+
+  const paginate = ({ selected: selectedPage }) =>
+    setCurrentPage(selectedPage + 1);
+  const pageCount = Math.ceil(filter_listings.length / listingsPerPage);
+  
     return (
       <div>
-        <Navbar user = {user}/> 
+        <Navbar user = {user} listing = {listings} searchListing = {searchListing} /> 
       <div className="home">
-      {listings.map(listing => (
+      {currentListings.map(listing => (
                 <Card key={listing.id} listing={listing} />
             ))}
       </div>
+      <div className="center">
+          <ReactPaginate
+            onPageChange={paginate}
+            pageCount={pageCount}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            containerClassName={"pagination"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item"}
+            nextLinkClassName={"page-link"}
+            activeClassName={"active"}
+          />
+        </div>
       </div>
     )
 }
