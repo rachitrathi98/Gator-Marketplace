160437ef25572f8b15e77388c0ba7ac620ff0cd9
MustafaGangardiwala <gangardiwalam.mg@gmail.com>
2022-03-25 18:50:59 -0400

    Added Pagination on Landing Page

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -2,11 +2,18 @@ import isAuth from "../helper/auth";
 import Card from "./Card";
 import Navbar from "./Navbar";
 import axios from "axios"
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import ReactPaginate from "react-paginate";
+import Loading from "../helper/LoadingSign";
+import "../style/loading.scss";
 
 const Landing = () => {
 
     const[listings, setListings] = useState([{}])
+    const [filter_listings, setFilterListings] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [listingsPerPage, setListingsPerPage] = useState(2);
+
     useEffect(async () => {
         const resp = await axios.get("http://localhost:8000/api/get-listings-landing")
          if(resp.data && resp.data.listings)
@@ -14,24 +21,57 @@ const Landing = () => {
             let lists = []
             resp.data.listings.map((listing) => lists.push(listing))
             setListings(resp.data.listings)
+            setFilterListings(resp.data.listings)
             console.log(listings)
          }
         }, []);
 
-    return (
-        <><Navbar user = {isAuth() ? isAuth().name : ""} />
-        
+const indexOfLastListing = currentPage * listingsPerPage;
+  const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+  const currentListings = filter_listings.slice(
+    indexOfFirstListing,
+    indexOfLastListing,
+  );
+
+  const paginate = ({ selected: selectedPage }) =>
+    setCurrentPage(selectedPage + 1);
+  const pageCount = Math.ceil(filter_listings.length / listingsPerPage);
+
+  let render = <Loading/>;
+
+  if(listings && listings.length > 0)
+  {
+    render = (
+        <Fragment>
+        <Navbar user = {isAuth() ? isAuth().name : ""} />
         <div className="home" id ="landing">
-        {listings.length>0?
+        {currentListings.length>0?
             listings.map(listing => (
                 <Card key={listing.id} listing={listing} />
             ))
             :<div></div>
             }
-        </div></>
-    
-
+        </div>
+        <div className="center">
+          <ReactPaginate
+            onPageChange={paginate}
+            pageCount={pageCount}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            containerClassName={"pagination"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item"}
+            nextLinkClassName={"page-link"}
+            activeClassName={"active"}
+          />
+        </div>
+        </Fragment>
     );
+    }
+ return render
 };
 
 export default Landing;
